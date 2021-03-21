7f7bdce05bb1d062ebd535fa1fa1332547a63398
Monika Wasilewska <monikawasilewska13@wp.pl>
2021-03-21 17:37:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const showModals = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal')
+
+
